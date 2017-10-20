fff370d40a1bda13d6061dcccecca879ceebf297
Gonzalo de Achaval <GonzaOK@gdeachaval.local>
2017-10-19 22:48:47 -0300

    Added search functionality

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -10,9 +9,10 @@ import java.util.Map;
  * @version 1.0
  */
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        List<String> phrases = Arrays.asList("hola mundo", "mundo", "hola");
-        //TODO falta que busque las phrases en un search.txt
+        List<String> phrases = readSearch(args);
+        //List<String> phrases = Arrays.asList("hola", "mundo");
         WordDetectionAutomaton nonDet = new WordDetectionAutomaton(phrases);
         WordDetectionAutomaton deterministic = nonDet.createDeterministic();
 
@@ -25,21 +25,45 @@ public class Main {
         FileManager fm = new FileManager();
         File[] files = fm.getHtmlFilesInDirectory();
 
-        String contentOfFile = "";
+        StringBuilder contentOfFile = new StringBuilder();
         for (String phrase : phrases) {
-            contentOfFile += "Sentence: " + phrase + "\n\n";
+            contentOfFile.append("Sentence: ").append(phrase).append("\n\n");
             for (File file : files) {
                 String content = fm.getContentOfFile(file);
                 final Map<String, Integer> frequencies = deterministic.getFrequencies(content);
 
                 if(frequencies.get(phrase) > 0){
-                    contentOfFile += "File name: " + file.getName() + "\n";
-                    contentOfFile += "Frequency: " + frequencies.get(phrase) + "\n";
+                    contentOfFile.append("File name: ").append(file.getName()).append("\n");
+                    contentOfFile.append("Frequency: ").append(frequencies.get(phrase)).append("\n");
                 }
             }
-            contentOfFile += "\n\n";
+            contentOfFile.append("\n\n");
         }
 
-        fm.writeToTextFile("index.txt", contentOfFile);
+        fm.writeToTextFile("index.txt", contentOfFile.toString());
+    }
+
+    private static List<String> readSearch(String[] args) throws IOException {
+        List<String> wordsToRead = new ArrayList<>();
+        String directory;
+        String name;
+        String fullPath;
+        try {
+            directory = args[0];
+            name = args[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new RuntimeException("You must run the program with two parameters: directory & filename");
+        }
+        fullPath = directory + "/" + name;
+        File file = new File(fullPath);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String word;
+        while((word = br.readLine()) != null) {
+            if(!wordsToRead.contains(word)){
+                wordsToRead.add(word);
+            }
+        }
+        return wordsToRead;
     }
 }
